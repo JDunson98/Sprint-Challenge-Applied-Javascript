0e529f7e58d37d4923faa7a552aaaf8266003122
JDunson98 <45635230+JDunson98@users.noreply.github.com>
2020-04-19 20:35:59 -0500

    finished step 2, stuck on step 3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,45 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response) => {
+        let articleLoop = response
+        const newCard = cardComponentCreator(articleLoop)
+            cardFinal.appendChild(newCard)
+        console.log(response)
+    })
+    .catch((err) => {
+        console.log('something went wrong', err)
+    })
+
+function cardComponentCreator(obj) {
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    let cardHeadline = document.createElement('div');
+    cardHeadline.classList.add('headline');
+    cardHeadline.textContent = obj.data.articles.headline;
+
+    let cardAuthor = document.createElement('div');
+    cardAuthor.classList.add('author');
+
+    let cardImgContainer = document.createElement('div');
+    cardImgContainer.classList.add('img-container');
+
+    let cardImg = document.createElement('img');
+    cardImg.src = obj.data.articles.authorPhoto;
+
+    let cardCredits = document.createElement('span');
+    cardCredits.textContent = 'By ' + obj.data.articles.authorName;
+
+    cardImgContainer.appendChild(cardImg);
+    cardAuthor.appendChild(cardImgContainer);
+    cardAuthor.appendChild(cardCredits);
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+
+    return card;
+}
+
+let cardFinal = document.querySelector('.cards-container');
+
