860e2d082dbd19afa7d6e25a6b4a9c2f064a87d0
JDunson98 <45635230+JDunson98@users.noreply.github.com>
2020-04-20 20:12:13 -0500

    finished step 3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -20,22 +20,59 @@
 // Use your function to create a card for each of the articles and add the card to the DOM.
 axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
     .then((response) => {
-        let articleLoop = response
-        const newCard = cardComponentCreator(articleLoop)
-            cardFinal.appendChild(newCard)
         console.log(response)
-    })
+
+        let javascriptNewHeadline = cardComponentCreator(response.data.articles.javascript[0].headline, response.data.articles.javascript[0].authorPhoto, response.data.articles.javascript[0].authorName)
+        let javascriptNewHeadline1 = cardComponentCreator(response.data.articles.javascript[1].headline, response.data.articles.javascript[1].authorPhoto, response.data.articles.javascript[1].authorName)
+        let javascriptNewHeadline2 = cardComponentCreator(response.data.articles.javascript[2].headline, response.data.articles.javascript[2].authorPhoto, response.data.articles.javascript[2].authorName)
+        let javascriptNewHeadline3 = cardComponentCreator(response.data.articles.javascript[3].headline, response.data.articles.javascript[3].authorPhoto, response.data.articles.javascript[3].authorName)
+
+        cardFinal.appendChild(javascriptNewHeadline)
+        cardFinal.appendChild(javascriptNewHeadline1)
+        cardFinal.appendChild(javascriptNewHeadline2)
+        cardFinal.appendChild(javascriptNewHeadline3)
+
+        let bootstrapHeadline = cardComponentCreator(response.data.articles.bootstrap[0].headline, response.data.articles.bootstrap[0].authorPhoto, response.data.articles.bootstrap[0].authorName)
+        let bootstrapHeadline1 = cardComponentCreator(response.data.articles.bootstrap[1].headline, response.data.articles.bootstrap[1].authorPhoto, response.data.articles.bootstrap[1].authorName)
+        let bootstrapHeadline2 = cardComponentCreator(response.data.articles.bootstrap[2].headline, response.data.articles.bootstrap[2].authorPhoto, response.data.articles.bootstrap[2].authorName)
+
+        cardFinal.appendChild(bootstrapHeadline)
+        cardFinal.appendChild(bootstrapHeadline1)
+        cardFinal.appendChild(bootstrapHeadline2)
+
+        let techHeadline = cardComponentCreator(response.data.articles.technology[0].headline, response.data.articles.technology[0].authorPhoto, response.data.articles.technology[0].authorName)
+        let techHeadline1 = cardComponentCreator(response.data.articles.technology[1].headline, response.data.articles.technology[1].authorPhoto, response.data.articles.technology[1].authorName)
+        let techHeadline2 = cardComponentCreator(response.data.articles.technology[2].headline, response.data.articles.technology[2].authorPhoto, response.data.articles.technology[2].authorName)
+
+        cardFinal.appendChild(techHeadline)
+        cardFinal.appendChild(techHeadline1)
+        cardFinal.appendChild(techHeadline2)
+
+        let jqueryHeadline =  cardComponentCreator(response.data.articles.jquery[0].headline, response.data.articles.jquery[0].authorPhoto, response.data.articles.jquery[0].authorName)
+        let jqueryHeadline1 =  cardComponentCreator(response.data.articles.jquery[1].headline, response.data.articles.jquery[1].authorPhoto, response.data.articles.jquery[1].authorName)
+        let jqueryHeadline2 =  cardComponentCreator(response.data.articles.jquery[2].headline, response.data.articles.jquery[2].authorPhoto, response.data.articles.jquery[2].authorName)
+
+        cardFinal.appendChild(jqueryHeadline)
+        cardFinal.appendChild(jqueryHeadline1)
+        cardFinal.appendChild(jqueryHeadline2)
+        
+        let nodeHeadline = cardComponentCreator(response.data.articles.node[0].headline, response.data.articles.node[0].authorPhoto, response.data.articles.node[0].authorName)
+        let nodeHeadline1 = cardComponentCreator(response.data.articles.node[1].headline, response.data.articles.node[1].authorPhoto, response.data.articles.node[1].authorName)
+
+        cardFinal.appendChild(nodeHeadline)
+        cardFinal.appendChild(nodeHeadline1)
+    }) 
     .catch((err) => {
         console.log('something went wrong', err)
     })
 
-function cardComponentCreator(obj) {
+function cardComponentCreator(headline, authorPhoto, authorName) {
     let card = document.createElement('div');
     card.classList.add('card');
 
     let cardHeadline = document.createElement('div');
     cardHeadline.classList.add('headline');
-    cardHeadline.textContent = obj.data.articles.headline;
+    cardHeadline.textContent = headline;
 
     let cardAuthor = document.createElement('div');
     cardAuthor.classList.add('author');
@@ -44,10 +81,10 @@ function cardComponentCreator(obj) {
     cardImgContainer.classList.add('img-container');
 
     let cardImg = document.createElement('img');
-    cardImg.src = obj.data.articles.authorPhoto;
+    cardImg.src = authorPhoto;
 
     let cardCredits = document.createElement('span');
-    cardCredits.textContent = 'By ' + obj.data.articles.authorName;
+    cardCredits.textContent = 'By ' + authorName;
 
     cardImgContainer.appendChild(cardImg);
     cardAuthor.appendChild(cardImgContainer);
